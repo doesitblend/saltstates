b5d168fb7bf8990ef2a45f5b04004d8124c2bbbe
Ken Jordan <ken.w.jordan@gmail.com>
2020-05-03 20:04:27 +0000

    updated mod in prep for production

--- prod/_modules/testmod.py
+++ prod/_modules/testmod.py
@@ -6,5 +6,5 @@ def mytestfunc():
     '''
     This is loaded from the master branch
     '''
-    msg = 'This is loaded from the master branch'
+    msg = 'This is loaded from the prod environment'
     return {'retcode': 0, 'comments': msg}
