87c9bfe7b68b6967c1c4c017263db7acae7b9c7f
Ken Jordan <ken.w.jordan@gmail.com>
2020-05-04 21:45:10 +0000

    Updated modules to determining source branch more clear when using

--- base/_modules/testmod.py
+++ base/_modules/testmod.py
@@ -2,6 +2,15 @@ import salt
 import salt.config
 
 
+__virtualname__ = 'testmod'
+
+def __virtual__():
+    '''
+    Test loading this module
+    '''
+    return __virtualname__
+
+
 def mytestfunc():
     '''
     This is loaded from the master branch
