9234373a22d9b43296c8ee25d4ff1b2be6f803df
Ken Jordan <ken.w.jordan@gmail.com>
2020-05-04 21:40:24 +0000

    Renamed for clarity of applicable branch

---
+++ staging/_modules/stagemod.py
@@ -0,0 +1,10 @@
+import salt
+import salt.config
+
+
+def mytestfunc():
+    '''
+    This is loaded from the master branch
+    '''
+    msg = 'This is loaded from the staging branch'
+    return {'retcode': 0, 'comments': msg}
